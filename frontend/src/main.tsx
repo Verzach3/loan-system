a8217ee204bbf2f089823c4978ad2bed5c505974
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2024-04-02 20:59:49 -0500

    Added router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MantineProvider>
-      <App />
+      <RouterProvider router={router}/>
     </MantineProvider>
   </React.StrictMode>
 );
